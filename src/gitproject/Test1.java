3932a4bc2b91774e077d0b3ebc65813f8ed1aafa
ramsingaryadav <151905673+ramsingaryadav@users.noreply.github.com>
2023-12-02 19:33:10 +0530

    First Commit

---
+++ src/gitproject/Test1.java
@@ -0,0 +1,18 @@
+package gitproject;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		
+		
+		if(hungry=="yes") {
+			System.out.println("eat something......");
+			
+		}else {
+			System.out.println("Do work......");
+		}
+
+	}
+
+}
